d86866769e3a488455f73998874f92aa37fa27f1
FabricioMF72 <55679079+FabricioMF72@users.noreply.github.com>
2021-01-16 15:36:41 -0600

    [ADD] Redux to Home Screen

--- ecommerce-frontend/src/screens/ProductList/ProductList.js
+++ ecommerce-frontend/src/screens/ProductList/ProductList.js
@@ -1,29 +1,20 @@
-import React, {useState, useEffect} from 'react';
+import React, { useEffect} from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import axios from 'axios';
 import ProductCard from '../../components/ProductCard/ProductCard';
 import Row from 'react-bootstrap/Row';
 import LoadingBox from '../../components/LoadingBox/LoadingBox'
 import MessageBox from '../../components/MessageBox/MessageBox'
 import "./ProductList.css";
+import { listProducts } from '../../actions/productActions';
 
 const ProductList = () => {
-    const [products, setProducts] = useState( [] );
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(false);
+    const dispatch = useDispatch();
+    const productList = useSelector( (state) => state.productList)
+    const {loading,error,products} = productList;
     useEffect(() =>{
-        const fecthdata = async () =>{
-            try {
-                setLoading(true);
-                const { data } = await axios.get('/api/products');
-                setProducts(data);;
-                setLoading(false)
-            } catch (err) {
-                setError(err.message);
-                setLoading(false)
-            }
-        };    
-        fecthdata();
-    }, [])
+        dispatch(listProducts());
+    }, [dispatch])
     return (
         <div>
             {loading? (<LoadingBox></LoadingBox>)
