9b54ad3e4669f642ad080a82d85ef0d6165aa8a3
FabricioMF72 <55679079+FabricioMF72@users.noreply.github.com>
2021-02-01 13:22:12 -0600

    [add] Place Order Action

--- ecommerce-frontend/src/screens/PlaceOrderScreen/PlaceOrderScreen.js
+++ ecommerce-frontend/src/screens/PlaceOrderScreen/PlaceOrderScreen.js
@@ -1,5 +1,9 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux'
+import { createOrder} from '../../actions/orderActions.js';
+import { ORDER_CREATE_RESET } from '../../constants/orderConstants';
+import LoadingBox from '../../components/LoadingBox/LoadingBox.js';
+import MessageBox from '../../components/MessageBox/MessageBox.js';
 import "./PlaceOrderScreen.css"
 import Col from 'react-bootstrap/Col'
 import Image from 'react-bootstrap/Image'
@@ -13,6 +17,8 @@ const PlaceOrderScreen = (props) => {
     if (!cart.paymentMethod) {
       props.history.push('/payment');
     }
+    const orderCreate = useSelector((state) => state.orderCreate);
+    const { loading, success, error, order } = orderCreate;
     const toPrice = (num) => Number(num.toFixed(2)); // 5.123 => "5.12" => 5.12
     cart.itemsPrice = toPrice(
       cart.cartItems.reduce((a, c) => a + c.qty * c.price, 0)
@@ -20,9 +26,16 @@ const PlaceOrderScreen = (props) => {
     cart.shippingPrice = cart.itemsPrice > 100 ? toPrice(0) : toPrice(10);
     cart.taxPrice = toPrice(0.15 * cart.itemsPrice);
     cart.totalPrice = cart.itemsPrice + cart.shippingPrice + cart.taxPrice;
+    const dispatch = useDispatch()
     const placeOrderHandler = () => {
-      console.log("HOLA")
+      dispatch(createOrder({...cart, orderItems: cart.cartItems}));
     };
+    useEffect(() => {
+        if (success) {
+          props.history.push(`/order/${order._id}`);
+          dispatch({ type: ORDER_CREATE_RESET });
+        }
+      }, [dispatch, order, props.history, success]);
     return (
         <div>
             <CheckoutSteps step1 step2 step3 step4></CheckoutSteps>
@@ -83,6 +96,8 @@ const PlaceOrderScreen = (props) => {
                          </Card.Text>
                         <Button onClick={placeOrderHandler} disabled={cart.cartItems.length === 0} variant="primary">PlaceOrder</Button>
                     </Card>
+                    {loading && <LoadingBox></LoadingBox>}
+                    {error && <MessageBox variant="danger">{error}</MessageBox>}
                 </Col>
             </Row>
         </div>
